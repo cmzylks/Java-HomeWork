0548cb5e960dc760fb7735f766716a21e5a80e64
cmzylsk <1024431898@qq.com>
2021-04-07 21:18:40 +0800

    附加题

---
+++ 05Lesson6683/src/java6683/sort/list/ListSort6683.java
@@ -0,0 +1,20 @@
+package java6683.sort.list;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+/**
+ * @author Elxect
+ * @date 2021/4/7 3:22 下午
+ */
+public class ListSort6683 extends Application {
+	public static void main(String[] args) {
+		launch(args);
+	}
+
+	@Override
+	public void start(Stage primaryStage) throws Exception {
+		primaryStage.setTitle("对象排序（List）");
+		primaryStage.show();
+	}
+}
---
+++ 05Lesson6683/src/java6683/sort/list/ListSort6683ViewController.java
@@ -0,0 +1,9 @@
+package java6683.sort.list;
+
+/**
+ * @author Elxect
+ * @date 2021/4/7 3:23 下午
+ */
+public class ListSort6683ViewController {
+
+}
