ab226991110ff0a63b9b2bbff0ecd3b0f160d315
13600893623 <aiwoxinsui1314>
2021-05-17 10:22:13 +0800

    课堂小测

--- 10Lesson6683/src/java6683/test/Demo.java
+++ 10Lesson6683/src/java6683/test/Demo.java
@@ -6,7 +6,13 @@ public class Demo {
     public static void main(String[] args) throws IOException {
         copyFile("a.txt", "temp.txt");
     }
-
+    // 【错误1】
+    // 行号: 7
+    // 原因: 未设置偏移量,浪费不必要的资源，
+    // 如果数组太大，则会对输出造成负担，如果太小则会导致从文件中读取的次数变多。
+    //         【错误2】
+    // 行号:11
+    // 原因:没有返回值，需要返回文件字节数
     public static long copyFile(String name1, String name2) throws IOException {
         long size = 0;
         InputStream is = new BufferedInputStream(new FileInputStream(name1));
